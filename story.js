ec830ef361428a49b495088a5434da56046e3f29
alwazzan13 <f.alwazzan1@gmail.com>
2021-02-01 19:55:48 +0300

    home work

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "ahmad"; 
+let age = 25;
+let hobby = "reding";
+
+
+
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`)
